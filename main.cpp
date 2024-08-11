f26df4f5bd2f26aaed29de322617f98bc28fcbe4
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-08-11 12:15:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,22 +6,22 @@ template <typename T>
 concept ComplexConcept = requires(const T v) {
 	{v.hash()}->std::convertible_to<long>;
 	{v.toString()}->std::same_as<std::string>;
-    } && !std::has_virtual_destructor<T>::value
+	} && !std::has_virtual_destructor<T>::value
 ;
 
 struct Task {
 	long hash() const {long w = 0; return w;};
-    std::string toString() const { std::string q = "Hello, world!"; return q;};
+	std::string toString() const { std::string q = "Hello, world!"; return q;};
 }; 
 
 void the_thing (ComplexConcept auto& t) {
 	long tmp1 = t.hash();
-    std::string tmp2 = t.toString();
+	std::string tmp2 = t.toString();
 }
 
 int main() {
-    Task task{};
-    the_thing(task);
+    	Task task{};
+    	the_thing(task);
 
-    return 0;
+    	return 0;
 }
